d7208b6fac9e8b3c9853af9656b0dbdad8473eff
Melissa Huang <melissahuang10@gmail.com>
2016-08-25 18:16:50 -0700

    Add new Dusk Poppy theme

--- app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeActivity.java
@@ -1,17 +1,48 @@
 package com.codepath.android.themeswitcher.app;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 
 public class ThemeActivity extends ActionBarActivity {
 
+    private Spinner spThemes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Utils.onActivityCreateSetTheme(this);
         setContentView(R.layout.activity_theme);
+        setupSpinnerItemSelection();
+    }
+
+    private void setupSpinnerItemSelection() {
+        spThemes = (Spinner) findViewById(R.id.spThemes);
+        spThemes.setSelection(ThemeApplication.currentPosition);
+        ThemeApplication.currentPosition = spThemes.getSelectedItemPosition();
+
+        spThemes.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                if (ThemeApplication.currentPosition != position) {
+                    Utils.changeToTheme(ThemeActivity.this, position);
+                }
+                ThemeApplication.currentPosition = position;
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 
 
---
+++ app/src/main/java/com/codepath/android/themeswitcher/app/ThemeApplication.java
@@ -0,0 +1,10 @@
+package com.codepath.android.themeswitcher.app;
+
+import android.app.Application;
+
+/**
+ * Created by melissahuang on 8/25/16.
+ */
+public class ThemeApplication extends Application {
+    public static int currentPosition;
+}
